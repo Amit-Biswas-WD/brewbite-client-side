cbefe67ad3fce1ea1aeea3c1a6f950a5e0528614
Amit biswas <amitbiswas9282@gmail.com>
2025-04-24 23:14:47 +0600

    PriceDropdownButton

---
+++ src/components/PriceDropdownButton/PriceDropdownButton.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const PriceDropdownButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleOptionClick = () => {
+    setIsOpen(false);
+  };
+  return (
+    <div className="relative inline-block text-left my-[68px]">
+      <button
+        onClick={toggleDropdown}
+        className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+      >
+        Price
+        <svg
+          className="-mr-1 ml-2 h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+
+      {isOpen && (
+        <div className="absolute -right-16 z-10 mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <div className="py-1">
+            <button
+              onClick={handleOptionClick}
+              className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Espresso
+            </button>
+            <button
+              onClick={handleOptionClick}
+              className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Latte
+            </button>
+            <button
+              onClick={handleOptionClick}
+              className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Cappuccino
+            </button>
+            <button
+              onClick={handleOptionClick}
+              className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Mocha
+            </button>
+            <button
+              onClick={handleOptionClick}
+              className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Cold Brew
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PriceDropdownButton;
+ 
--- src/pages/Coffees/Coffees.jsx
+++ src/pages/Coffees/Coffees.jsx
@@ -1,10 +1,12 @@
 import CategoryDropdownButton from "../../components/CategoryDropdownButton/CategoryDropdownButton";
+import PriceDropdownButton from "../../components/PriceDropdownButton/PriceDropdownButton";
 
 
 const DropdownButton = () => {
   return (
     <div className="container mx-auto">
       <CategoryDropdownButton/>
+      <PriceDropdownButton/>
     </div>
   );
 };
