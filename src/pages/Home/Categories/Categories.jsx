ecb5cac2c981a2a60759844fb1ca71004e5cb9e8
Amit biswas <amitbiswas9282@gmail.com>
2025-04-28 14:12:41 +0600

    Categories swiper

--- src/pages/Home/Categories/Categories.jsx
+++ src/pages/Home/Categories/Categories.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import { Parallax } from "react-parallax";
 import "./Categories.css";
 import image from "../../../../public/category/image.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
@@ -23,26 +27,37 @@ const Categories = () => {
         <h2 className="text-3xl font-bold text-center mb-6 h2">
           Explore Categories
         </h2>
-        <div className="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-          {categories.map((cat, index) => (
-            <div
-              key={index}
-              className="relative group text-center transition-all overflow-hidden shadow flex flex-col justify-center items-center"
-              style={{
-                backgroundImage: `url(${cat.image})`,
-                height: "300px",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-            >
-              <h3 className="text-white text-lg font-semibold z-10 relative">
-                {cat.name}
-              </h3>
-              <div className="absolute inset-0 bg-[#1b1a1a] opacity-60 group-hover:bg-opacity-0 group-hover:translate-y-full transition-all duration-300 ease-in-out z-0"></div>
-            </div>
+        <Swiper
+          slidesPerView={5}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          modules={[Pagination, Autoplay]}
+        >
+          {categories.map((cat) => (
+            <SwiperSlide key={cat._id}>
+              <div
+                className="relative group text-center transition-all overflow-hidden shadow flex flex-col justify-center items-center"
+                style={{
+                  backgroundImage: `url(${cat.image})`,
+                  height: "300px",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <h3 className="text-white text-lg font-semibold z-10 relative">
+                  {cat.name}
+                </h3>
+                <div className="absolute inset-0 bg-[#1b1a1a] opacity-60 group-hover:bg-opacity-0 group-hover:translate-y-full transition-all duration-300 ease-in-out z-0"></div>
+              </div>
+            </SwiperSlide>
           ))}
-        </div>
-
+        </Swiper>
         <div className="text-center mt-8">
           <Link to={`/allCoffees`}>
             <button className="bg-[#7b4f29] hover:bg-[#5c3c20] text-white py-2 px-6 rounded-full">
