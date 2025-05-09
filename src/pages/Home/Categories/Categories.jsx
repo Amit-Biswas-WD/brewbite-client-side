1128af379cb94f054b74dbb939ce565e468c1cb2
mithunbinfo <mithunb.info@gmail.com>
2025-05-09 19:53:15 +0600

    category 2

--- src/pages/Home/Categories/Categories.jsx
+++ src/pages/Home/Categories/Categories.jsx
@@ -12,7 +12,9 @@ const Categories = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("https://brewbite-server-side-ca7s88vlk-amit6.vercel.app/category")
+    fetch("https://brewbite-server-side-ca7s88vlk-amit6.vercel.app/category", {
+      credentials: "include",
+    })
       .then((res) => res.json())
       .then((data) => setCategories(data));
   }, []);
